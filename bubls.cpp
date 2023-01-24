e204bd03f88a201ff9d685dc937358dc52dc82e6
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-01-24 08:53:49 +0300

    Create bubls.cpp

---
+++ bubls.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+main()
+{
+	int mas [100];
+	int n;
+	
+	printf ("kol element: ");
+	scanf ("%i", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%i", &mas [i]);
+	}
+	
+	for (int i = 0; i < n; i++)
+	
+	{
+		printf("%4i", mas [i]);
+	}	
+	
+	printf("\n");
+	
+	for(int j=0;j<n;j++)
+	{
+	
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (mas [i]> mas [i+1])
+			{
+				int z = mas [i];
+				mas [i] = mas [i+1];
+				mas [i+1] = z;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		printf("%4i",mas [i]);
+	}	
+}
+
+
